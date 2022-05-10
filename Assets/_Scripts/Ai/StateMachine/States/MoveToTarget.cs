d7e07ce79b011d2555c089b6a135e3f9bda0aa2d
eLilyPad <elizabethbryce00@gmail.com>
2022-05-10 19:34:25 +0100

    MovementSmooting

--- Assets/_Scripts/Ai/StateMachine/States/MoveToTarget.cs
+++ Assets/_Scripts/Ai/StateMachine/States/MoveToTarget.cs
@@ -4,11 +4,16 @@ namespace Lily.Ai.ActionStates
 {
 	using Ai.Pathfinder;
 	using Ai;
-	public class MoveToTarget : IState
+  using Lily.Utilities;
+  using System.Collections.Generic;
+
+  public class MoveToTarget : IState
 	{
 		private BasicAI _ai;
 		private readonly Rigidbody _rb;
 
+		int numSamplesForSmoothing = 5;
+    Queue<Vector3> velocitySamples = new Queue<Vector3>();
 
 		float MsTurnPenalty = 1;
 		public MoveToTarget(BasicAI ai, Rigidbody rb)
@@ -36,12 +41,24 @@ namespace Lily.Ai.ActionStates
 		{
 			Vector3 direction = new Vector3(_ai.waypoint.x, _ai.transform.position.y, _ai.waypoint.z);
 			// direction.Normalize();
-			_ai.transform.LookAt(direction);
+			LookAtDirection(MathsUtils.SmoothSamples(_ai.waypoint));
 			//RotateYToTarget(_ai.waypoint, _ai.turnSpeed, false);
+		}
+
+		void LookAtDirection(Vector3 direction)
+		{
+			direction.Normalize();
+
+			if (direction.sqrMagnitude > 0.001f)
+      {
+				float toRotation = (Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg);
+        float rotation = Mathf.LerpAngle(_rb.rotation.eulerAngles.y, toRotation, Time.deltaTime * _ai.turnSpeed);
+        
+				_rb.rotation = Quaternion.Euler(0, rotation, 0);
+			}
 		}
 		void MoveForward()
 		{
-			Vector3 direction = new Vector3(-1,0,0);
 			_ai.transform.Translate(Vector3.forward * Time.deltaTime * GetSpeed(), Space.Self);
 			//_rb.AddRelativeForce((Vector3.forward * _ai.acceleration * Time.deltaTime) * 100, ForceMode.Acceleration);
 		}
@@ -54,10 +71,15 @@ namespace Lily.Ai.ActionStates
 		float GetSpeed()
 		{
 			float movementSpeed = _ai.acceleration;
-			if(IsLookingAt(_ai.waypoint))
+			if(IsLookingAt(_ai.waypoint, 10))
 			{
-				movementSpeed = movementSpeed/2;
+				movementSpeed = _ai.acceleration;
 			}
+			else
+			{
+				movementSpeed = _ai.acceleration/2;
+			}
+
 			return movementSpeed;
 		}
 		bool IsLookingAt(Vector3 target, float angleThreshold = 0.5f)
